0f645c6cbf4fe066466018e775aaa0cc14308bde
haytam <haytamraba11@gmail.com>
2025-03-11 23:14:25 +0000

    poo/java 00 : methods,scope var, overloaded methods

--- java/fundamentals/Main.java
+++ java/fundamentals/Main.java
@@ -1,4 +1,4 @@
-package fundamentals;
+
 // strings
 
 public class Main{
---
+++ java/poo/java00.java
@@ -0,0 +1,22 @@
+
+public class java00{
+  
+  static int varClass=3;
+  public static void main(String[] args) {
+    // -method : a block of reusable code that is executed when called
+    // -overloaded methods : methods that share the same name but different
+    //                      parameters
+    // -variable scope : where a variable can be accessed
+    //                  local && class
+    double varlocal=1;
+    System.out.println( add(varlocal,2) + add(2,2,2) );  
+
+  }
+  static double add(double a, double b){
+    return a + b;
+  }
+  static double add(double a, double b, double c){
+    return a+b+c;
+  }
+
+}
