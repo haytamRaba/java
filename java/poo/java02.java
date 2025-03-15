172c7536e7b44250650188d3e091f3b6e3283add
haytam <haytamraba11@gmail.com>
2025-03-15 01:49:19 +0000

    java02 : constructor | this

---
+++ java/poo/java02.java
@@ -0,0 +1,21 @@
+public class java02 {
+  public static void main(String[] args) {
+    // constructor = a special method to initialize objects
+    //              you can pass arguments to a constructor
+    //              and set up initial values
+    // this  is a reference to the current object
+
+    java02_student student = new java02_student("haytam",20, 15);
+    System.out.println(student.name + " " + student.age + " " + student.note + " " + student.isEnrolled);
+    student.study();
+
+
+
+
+
+
+
+
+  }
+  
+}
---
+++ java/poo/java02_student.java
@@ -0,0 +1,18 @@
+public class java02_student {
+  String name;
+  int age;
+  double note;
+  boolean isEnrolled;  
+
+  java02_student( String name, int age, double note ){
+    this.name = name;
+    this.age = age;
+    this.note = note;
+    this.isEnrolled = true;
+  }
+
+  void study(){
+    System.out.println(this.name + " is studying");
+  }
+
+}
