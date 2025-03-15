9768c4d7b6c8ebaa887658b0938fe3d472831b13
haytam <haytamraba11@gmail.com>
2025-03-15 01:27:16 +0000

    poo/java01: basics

---
+++ java/poo/java01.java
@@ -0,0 +1,20 @@
+public class java01 {
+  public static void main(String [] args){
+
+    //object = an entity that holds data "attributes"
+    //         and can perform actions "methods"
+    //         it is a reference data type
+
+    java01_Car car =new java01_Car();
+    
+    car.drive();
+    car.brake();
+
+
+
+
+
+
+  }
+  
+}
