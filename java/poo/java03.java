318f480542d5eefab1508b4cb8123bc0261f89f0
haytam <haytamraba11@gmail.com>
2025-03-15 02:16:27 +0000

    java03 : overloaded constructors

---
+++ java/poo/java03.java
@@ -0,0 +1,18 @@
+public class java03 {
+  public static void main(String[] var0) {
+     // Create instances of java03_user with different constructors
+     java03_user var1 = new java03_user("haytam");
+     java03_user var2 = new java03_user("HAYTAM", "haytamraba11@gmail.com");
+     java03_user var3 = new java03_user("HAYTAM", "axyzef@ghijklm.nop", 90);
+     java03_user var4 = new java03_user();
+
+     // Print user details
+     System.out.println(var1.username + " " + var1.email + " " + var1.age);
+     System.out.println("user 2");
+     System.out.println(var2.username + " " + var2.email + " " + var2.age);
+     System.out.println("user 3");
+     System.out.println(var3.username + " " + var3.email + " " + var3.age);
+     System.out.println("user 4");
+     System.out.println(var4.username + " " + var4.email + " " + var4.age);
+  }
+}
---
+++ java/poo/java03_user.java
@@ -0,0 +1,33 @@
+public class java03_user{
+
+  String username;
+  String email;
+  int age;
+//overloaded constructors
+
+  java03_user(){
+    this.username = "guest";
+    this.email = " not provided ";
+    this.age = 0 ;
+  }
+    java03_user(String username){
+    this.username = username;
+    this.email = " not provided ";
+    this.age = 0 ;
+  }
+  java03_user(String username,String email){
+    this.username = username;
+    this.email = email;
+    this.age = 0 ;
+
+  }
+  java03_user(String username,String email,int age ){
+    this.username = username;
+    this.email = email;
+    this.age = age ;
+
+  }
+
+
+
+}
