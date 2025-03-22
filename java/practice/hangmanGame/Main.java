176e2cc7785c0dfaa84bf7e254dc7f2b4acd6a5d
haytam <haytamraba11@gmail.com>
2025-03-22 02:59:29 +0000

    hangman using files using random using try catch ofcourse

--- java/practice/hangmanGame/Main.java
+++ java/practice/hangmanGame/Main.java
@@ -1,17 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-   // java Hangman Game    
+   // java Hangman Game   
    
-   String word ="pizza";
+   String filePath = "words.txt";
+   Random random;
+   ArrayList<String> words = new ArrayList<>();
+
+   try (BufferedReader reader = new BufferedReader(new FileReader(filePath)) ){
+    String line;
+
+    while( ( line = reader.readLine() ) != null ){
+      // spaces : trim()
+      words.add(line.trim());
+    }
+
+   }
+   catch( FileNotFoundException e ){
+    System.out.println("could not find file");
+
+   }
+   catch(IOException e) {
+    System.out.println("something went wrong");
+   }
+
+   random = new Random();
+   // random number between 0 and length of words
+
+   String word = words.get( random.nextInt( words.size() ) );
+
+  //  System.out.println(words);
+
+   
+
 
    Scanner sc = new Scanner(System.in);
 
    ArrayList <Character> wordstate = new ArrayList<>();
    int wrongGuesses = 0 ;
 
+  
    for(int i = 0 ; i<word.length() ; i++){
     wordstate.add('-');
    }
@@ -20,26 +55,36 @@ public class Main {
    System.out.println("              java hangman           ");
    System.out.println("*  * * *  * * *  * *  * *  * * *   *");
 
+   // while guesses less of the length of the correct word 
    while(wrongGuesses < 6 )
    {
         System.out.print(getHangmanArt(wrongGuesses));
         System.out.print("Word : ");
-
+          // print the word length f word
         for(char c : wordstate){
           System.out.print(c+ " ");
         }
       
         System.out.println();
         System.out.print("Guess a letter : ");
+        // take the caracter
         char guess = sc.next().toLowerCase().charAt(0);
       
+        // verify if the caacter in the orignal word
         if(word.indexOf(guess) >= 0){
+
           System.out.println("correct guess!");
+
           for(int i =0;i < word.length() ; i++ ){
-            if(word.charAt(i) == guess){
-              wordstate.set(i, guess);
-            }
+            // replace each correct carcter in the right place 
+               if(word.charAt(i) == guess){
+
+                  wordstate.set(i, guess);
+                
+                }
           }
+          // if word dont contain any _ so the word is correct
+          // because we replace them 
           if(!wordstate.contains('-')){
             System.out.println(getHangmanArt(wrongGuesses));
             System.out.println("You Win!");
@@ -49,6 +94,7 @@ public class Main {
       
         }
         else{
+          // wrong caracter 
           System.out.println("wrong guess!");
           wrongGuesses++;
         }
@@ -62,6 +108,7 @@ public class Main {
    System.out.println();
   // System.out.println(wordstate);
    sc.close();
+  
   }
 
   static String getHangmanArt(int wrongGuesses){
